e729bdfca0007ff93a05d34487436eed9fbda19c
PRANAV N RAO <48349523+pranavanrao@users.noreply.github.com>
2020-09-14 20:55:32 +0530

    Add files via upload

---
+++ Control-flow/app.py
@@ -0,0 +1,26 @@
+# Conditional statements
+
+# ternary operators
+temperature = 15
+if temperature > 30:
+    print("It's warm, drink water")
+elif temperature > 20:
+    print("It's cold")
+else:
+    print("It's freezing")
+print("Anyways, drink water!")
+
+# alternative to ternary operator
+age = 22
+message = "Eligible" if age >= 18 else "Not elegible"
+print(message)
+
+# logical operators
+high_income = False
+good_credit = True
+student = False
+
+if (high_income or good_credit) and not student:
+    print('Eligible')
+else:
+    print('Not eligible')
---
+++ Variables/app.py
@@ -0,0 +1,29 @@
+# variables
+number = 10
+print(number) #prints the variable value
+
+# string
+name = "pranav n rao"
+print(len(name)) # prints the length of the string
+print(name) # prints the string
+print(name[0]) #prints the string in that perticular index
+print(name[-1])
+print(name[0:4]) 
+print(name[0:])
+
+# String methods
+print(name.upper())
+print(name.title())
+print(name.find('rao'))
+print(name.replace('r', 'j'))
+print('rao' in name)
+print('roa' not in name)
+
+# Numbers
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 // 3)
+print(10 ** 3)
+print(10 % 3)
